87dcd33a2937a32abebd082bf3cf6e13a74b451d
Scott Abbott <scott.abbott.a@gmail.com>
2023-03-01 22:35:47 -0700

    Gitsigns to visually see changes in buffer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,6 +44,13 @@ return require("packer").startup(function(use)
   }
 
   use {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  use {
     "theblob42/drex.nvim",
     requires = "kyazdani42/nvim-web-devicons", -- optional
   }
