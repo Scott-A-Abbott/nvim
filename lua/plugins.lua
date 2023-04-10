a54aaecfb64a2b2f7c4882314be0151762106b18
Scott Abbott <scott.abbott.a@gmail.com>
2023-04-10 13:30:29 -0600

    Lualine config is updated

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,9 @@
 function relative_path()
-  local root = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-  local path = vim.fn.fnamemodify(vim.fn.expand('%'), ':~:.')
-  return root .. '/' .. path
+  return vim.fn.fnamemodify(vim.fn.expand('%'), ':~:.')
+end
+
+function current_directory()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
 end
  
 return require("packer").startup(function(use)
@@ -107,19 +109,20 @@ return require("packer").startup(function(use)
     requires = { "kyazdani42/nvim-web-devicons", "f-person/git-blame.nvim"},
     config = function()
       vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+      vim.g.gitblame_message_template = '<author> • <date>'
       local git_blame = require('gitblame')
 
       require "lualine".setup({
         extensions = { "nvim-tree", "drex" },
         options = { theme = "ayu" },
         sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch'},
+          lualine_a = { 'mode' },
+          lualine_b = { current_directory },
           lualine_c = { relative_path },
           lualine_x = {
             { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
           },
-          lualine_y = {'progress'},
+          lualine_y = {'branch'},
           lualine_z = {'location'}
         },
       })
