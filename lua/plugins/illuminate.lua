1a15c1af3e84a2dc862a918523900810c45fbebd
Scott Abbott <scott.abbott.a@gmail.com>
2023-06-12 12:36:16 -0600

    Illuminate gui is updated

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,1 +1,8 @@
-return { 'RRethy/vim-illuminate' }
+return {
+  'RRethy/vim-illuminate',
+  config = function()
+    vim.cmd("hi IlluminatedWordText gui=NONE guibg=#323a4c")
+    vim.cmd("hi IlluminatedWordRead gui=NONE guibg=#323a4c")
+    vim.cmd("hi IlluminatedWordWrite gui=NONE guibg=#323a4c")
+  end
+}
