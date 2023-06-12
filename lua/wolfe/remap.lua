4affa7927e5d039f8e3c65986cf4abbd8711bb22
Scott Abbott <scott.abbott.a@gmail.com>
2023-06-11 20:23:39 -0600

    Vestigial comments are removed

--- lua/wolfe/remap.lua
+++ lua/wolfe/remap.lua
@@ -1,18 +1,9 @@
--- Telescope
--- local builtin = require "telescope.builtin"
--- vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
--- vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
--- vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
--- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
--- vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, {})
-
-local wk = require("which-key")
-
 -- Visual Multi remap
 local keymap_options = { noremap = true }
 vim.api.nvim_set_keymap("n", "<C-d>", "<Plug>(VM-Add-Cursor-Down)", keymap_options)
 vim.api.nvim_set_keymap("n", "<C-u>", "<Plug>(VM-Add-Cursor-Up)", keymap_options)
 
+local wk = require("which-key")
 wk.register({
   ["?"] = { "<CMD>WhichKey<CR>", "Keys" },
   ["<leader>"] = {
