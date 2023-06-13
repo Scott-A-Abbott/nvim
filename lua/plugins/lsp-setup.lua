86e9db5976e34a0eecd7f04c0d6b7609b4d9625e
Scott Abbott <scott.abbott.a@gmail.com>
2023-06-13 13:22:59 -0600

    LSP default keymaps

--- lua/plugins/lsp-setup.lua
+++ lua/plugins/lsp-setup.lua
@@ -11,18 +11,25 @@ return {
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup()
-
-    local rt = require("rust-tools")
-    rt.setup({
-      server = {
-        on_attach = function(_, bufnr)
-          -- Code action groups
-          vim.keymap.set("n", "<C-.>", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-      },
-    })
+    require("rust-tools").setup()
 
     require('lsp-setup').setup({
+      default_mappings = false,
+      mappings = {
+        gD = 'lua vim.lsp.buf.declaration()',
+        gd = 'lua vim.lsp.buf.definition()',
+        gt = 'lua vim.lsp.buf.type_definition()',
+        gi = 'lua vim.lsp.buf.implementation()',
+        gr = 'lua vim.lsp.buf.references()',
+        -- ['<C-k>'] = 'lua vim.lsp.buf.signature_help()',
+        ['<space>rn'] = 'lua vim.lsp.buf.rename()',
+        ['<space>ca'] = 'lua vim.lsp.buf.code_action()',
+        ['<space>ch'] = 'lua vim.lsp.buf.hover()',
+        ['<space>f'] = 'lua vim.lsp.buf.formatting()',
+        ['<space>e'] = 'lua vim.diagnostic.open_float()',
+        ['[d'] = 'lua vim.diagnostic.goto_prev()',
+        [']d'] = 'lua vim.diagnostic.goto_next()',
+      },
       servers = {
         bashls = {},
         cssls = {},
