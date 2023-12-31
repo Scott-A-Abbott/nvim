6d9d706de2db50f8c986be104df32f909ab69aea
Scott Abbott <scott.abbott.a@gmail.com>
2023-12-31 14:29:48 -0700

    Display border for LSP floating panes

--- lua/wolfe/config.lua
+++ lua/wolfe/config.lua
@@ -70,3 +70,22 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 vim.diagnostic.config({
   virtual_text = false,
 })
+
+-- Display border to LSP floating panes
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
