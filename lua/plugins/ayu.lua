9b76efeabe46ce3b01a435401a1d881adf5a21fc
Scott Abbott <scott.abbott.a@gmail.com>
2024-06-04 10:31:43 -0600

    SpellBad is red

--- lua/plugins/ayu.lua
+++ lua/plugins/ayu.lua
@@ -16,7 +16,8 @@ return {
         WhichKeyFloat = { bg = "#333C52" },
         Comment = { fg = "#8691A3" },
         TreesitterContext = { bg = "#333C52" },
-        NormalNC = {bg = '#191d26', fg = '#CBCCC6'}
+        NormalNC = {bg = '#191d26', fg = '#CBCCC6'},
+        SpellBad = { fg = "#f20404" }
       }, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
     })
 
