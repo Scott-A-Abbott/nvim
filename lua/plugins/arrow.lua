af61b5bcd82bef2bc12e1f87d0a49247e0be151d
Scott Abbott <scott.abbott.a@gmail.com>
2024-09-26 10:11:21 -0600

    Arrow plugin

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,14 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  opts = {
+    show_icons = true,
+    always_show_path = true,
+    leader_key = ';', -- Recommended to be a single key
+    buffer_leader_key = 'm', -- Per Buffer Mappings
+    index_keys = "123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP", -- Update this
+    mappings = {
+      next_item = ">",
+      prev_item = "<"
+    }
+  }
+}
