ffc65a2fd726477733142abada8ec8b2ee629466
Scott Abbott <scott.abbott.a@gmail.com>
2024-09-26 10:14:50 -0600

    Arrow leader key is updated

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -3,8 +3,8 @@ return {
   opts = {
     show_icons = true,
     always_show_path = true,
-    leader_key = ';', -- Recommended to be a single key
-    buffer_leader_key = 'm', -- Per Buffer Mappings
+    leader_key = "'", -- Recommended to be a single key
+    buffer_leader_key = "m", -- Per Buffer Mappings
     index_keys = "123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP", -- Update this
     mappings = {
       next_item = ">",
