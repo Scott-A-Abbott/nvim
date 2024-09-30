25297ba52aaf241225936176ce813f2356e5a280
Scott Abbott <scott.abbott.a@gmail.com>
2024-09-29 21:38:51 -0600

    Illuminate plugin is removed

--- lua/plugins/illuminate.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'RRethy/vim-illuminate',
-  config = function()
-    vim.cmd("hi IlluminatedWordText gui=NONE guibg=#323a4c")
-    vim.cmd("hi IlluminatedWordRead gui=NONE guibg=#323a4c")
-    vim.cmd("hi IlluminatedWordWrite gui=NONE guibg=#323a4c")
-  end
-}
