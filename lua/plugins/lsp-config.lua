ebc103963a310add923a05d9c9ec6121b7b9147c
Scott Abbott <sabbott@wsgr.com>
2025-10-13 22:26:56 -0600

    LSP config, rather than LSP setup

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,12 @@
+return {
+  'neovim/nvim-lspconfig',
+  lazy = false,
+  config = function ()
+    vim.lsp.config('rust_analyzer', {
+      -- Server-specific settings. See `:help lsp-quickstart`
+      settings = {
+        ['rust-analyzer'] = {},
+      },
+    })
+  end,
+}
